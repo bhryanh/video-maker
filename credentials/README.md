9a542f4b567f82d0ffb2b4052f0b56f3f010c021
bhryan <bhryan@casoca.com.br>
2019-05-01 16:17:34 -0300

    text robot: adding watson IA natural language

--- credentials/README.md
+++ credentials/README.md
@@ -9,3 +9,18 @@ File: `algorithmia.json`
     "apiKey": "API KEY HERE"
 }
 ```
+
+## Watson Natural Language Understanding
+
+File: `watson-nlu.json`
+
+```
+{
+  "apikey": "APIK KEY",
+  "iam_apikey_description": "Auto-generated for key ...",
+  "iam_apikey_name": "Auto-generated service credentials",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/...::serviceid:ServiceId-....",
+  "url": "https://gateway.watsonplatform.net/natural-language-understanding/api"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -2,6 +2,15 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js')
+
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
+})
+
 async function robot(content) {
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -48,9 +57,40 @@ async function robot(content) {
         })
     }
 
+    async function limitMaximumSentences(content){
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence){
+        return new Promise((resolve,reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if (error) {
+                    throw error
+                }
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
+                resolve(keywords)
+            })
+        })
+    }
+
+    async function fetchKeywordsOfAllSentences(content){
+        for(let sentence of content.sentences){
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
     await fetchContentFromWikipedia(content)
     await sanitizeContent(content)
     await breakContentIntoSentences(content)
+    await limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content) 
 
 
 }
